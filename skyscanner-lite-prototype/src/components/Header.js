bc1a8bdc5e11c5b932ef9183ade286b7fdac53b3
Wajeeh Syed <wsyed019@uottawa.ca>
2025-06-08 21:14:29 -0400

    React Service Site

---
+++ skyscanner-lite-prototype/src/components/Header.js
@@ -0,0 +1,22 @@
+import React from 'react';
+
+function Header() {
+    return (
+        <header>
+            <nav className="navbar navbar-expand-lg bg-light shadow-sm">
+                <div className="container">
+                    <a className="navbar-brand fs-4" href="HomeScreen.js"><i className="bi bi-airplane-engines-fill"></i> G36 Air</a>
+                    
+                    <div className="ms-auto">
+                        <a href="https://syed04wajeeh.github.io/My-Portfolio/" className="btn btn-outline-primary">
+                            <i className="bi bi-person-circle me-2"></i>
+                            My Portfolio
+                        </a>
+                    </div>
+                </div>
+            </nav>
+        </header>
+    );
+}
+
+export default Header;
